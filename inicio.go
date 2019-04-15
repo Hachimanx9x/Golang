0da8091868a7218c5430152ca8967523e8c8c712
Hachimanx9x <bolanosd38@gmail.com>
2019-04-15 16:09:17 -0500

    integrando archicos .html

--- inicio.go
+++ inicio.go
@@ -1,10 +1,23 @@
 package main
 import (
-	"fmt"
-  "net/http"
-  "strings"
+	"html/template"
+	"fmt" //esto se usa para imprimir 
+ "net/http"   //crear las entradas y definir puerto
+
 )
+//contenido
+func index( w http.ResponseWriter, r  *http.Request){
+	//fmt.Fprintf(w, "holi")
+	template, err := template.ParseFiles("Archivos html/indes.html")
+	if err != nil{
+		fmt.Fprintf(w, "mala suerte perro")
+	}else{
+	template.Execute(w,nil)	
+	}
+}
+
 func main(){
+	http.HandleFunc("/", index)
 	fmt.Println("el server")
-	
+	http.ListenAndServe(":5000", nil)
 }
